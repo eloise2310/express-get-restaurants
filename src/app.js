40c6d793e78cb0355e5fd9d172d3019f70431701
eloise <eloisewilkinson@Eloises-MacBook-Pro.local>
2024-10-15 16:14:13 +0100

    finsihed

--- src/app.js
+++ src/app.js
@@ -5,12 +5,34 @@ const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.get("/restaurants", async (request, response) => {
     const restaurants = await Restaurant.findAll({});
     response.json(restaurants);
 })
 
+app.get("/restaurants/:id", async (request, response) => {
+    const number = request.params.id;
+    const restaurant = await Restaurant.findByPk(number);
+    response.json(restaurant)
+})
 
+app.post("/restaurants", async (request, response) => {
+    const newRestaurant = await Restaurant.create(request.body);
+    response.json(newRestaurant)
+})
+
+app.put("/restaurants/:id", async (request, response) => {
+    const updatedRestaurants = await Restaurant.update(request.body, {where: {id: request.params.id}});
+    response.json(updatedRestaurants);
+})
+
+app.delete("/restaurants/:id", async (request, response) => {
+    const deletedRestaurant = await Restaurant.destroy({where: {id: request.params.id}});
+    response.json(deletedRestaurant)
+})
 
 
 module.exports = app;
