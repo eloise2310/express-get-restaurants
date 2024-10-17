45bccdb5f536c0b0925f54f7bb3654c46143cf85
eloise <eloisewilkinson@Eloises-MacBook-Pro.local>
2024-10-17 14:43:37 +0100

    day 4 complete tests pass

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -2,11 +2,15 @@ const express = require("express");
 const app = express();
 const restaurantRouter = express.Router();
 const Restaurant = require("../models/Restaurant");
-const db = require('../db/connection');
-const { where } = require("sequelize");
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+// include check and validationResult
+const { check, validationResult } = require("express-validator");
+
+// const { router } = require("../src/app");
+
+restaurantRouter.use(express.json());
+restaurantRouter.use(express.urlencoded({extended: true}));
+
 
 restaurantRouter.get("/", async (request, response) => {
     const restaurants = await Restaurant.findAll({});
@@ -19,11 +23,11 @@ restaurantRouter.get("/:id", async (request, response) => {
     response.json(restaurant)
 })
 
-restaurantRouter.post("/", async (request, response) => {
-    const restaurant = await Restaurant.create(request.body);
-    const newRestaurant = await Restaurant.findAll({})
-    response.json(newRestaurant)
-})
+// restaurantRouter.post("/", async (request, response) => {
+//     const restaurant = await Restaurant.create(request.body);
+//     const newRestaurant = await Restaurant.findAll({})
+//     response.json(newRestaurant)
+// })
 
 restaurantRouter.put("/:id", async (request, response) => {
     const updatedRestaurants = await Restaurant.update(request.body, {where: {id: request.params.id}});
@@ -37,5 +41,64 @@ restaurantRouter.delete("/:id", async (request, response) => {
     response.json(restaurants)
 })
 
+
+
+// //check for name 
+// restaurantRouter.post("/", [check("name").not().isEmpty().trim()], (req, res) => {
+//     const errors = validationResult(req);
+//     if(!errors.isEmpty()){
+//         res.json({error: errors.array()})
+//     }
+//     else{
+//         const newRestaurant = req.body;
+//         restaurants.push(newRestaurant);
+//         res.json(restaurants)
+//     }
+// })
+
+// //check for location
+// restaurantRouter.post("/", [check("location").not().isEmpty().trim()], (req, res) => {
+//     const errors = validationResult(req);
+//     if(!errors.isEmpty()){
+//         res.json({error: errors.array()})
+//     }
+//     else{
+//         const newRestaurant = req.body;
+//         restaurants.push(newRestaurant);
+//         res.json(restaurants)
+//     }
+// })
+
+// //check cuisine 
+// restaurantRouter.post("/", [check("cuisine").not().isEmpty().trim()], (req, res) => {
+//     const errors = validationResult(req);
+//     if(!errors.isEmpty()){
+//         res.json({error: errors.array()})
+//     }
+//     else{
+//         const newRestaurant = req.body;
+//         restaurants.push(newRestaurant);
+//         res.json(restaurants)
+//     }
+// })
+
+// checks all post
+restaurantRouter.post("/", 
+    [
+        check("name").not().isEmpty().trim().withMessage("Name is required"),
+        check("location").not().isEmpty().trim().withMessage("Location is required"),
+        check("cuisine").not().isEmpty().trim().withMessage("Cuisine is required")
+    ],
+    async (request, response) => {
+    const errors =validationResult(request);
+        if(!errors.isEmpty()){
+            response.json({error: errors.array()})
+        }
+        else{
+            const restaurant = await Restaurant.create(request.body);
+            const newRestaurant = await Restaurant.findAll({})
+            response.json(newRestaurant)
+        }
+})
 
 module.exports = restaurantRouter
